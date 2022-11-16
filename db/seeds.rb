5802f9bdefcadf20d3da0135d4f5202b709adf94
Denis Velic <denis.velic@laposte.net>
2022-11-16 16:46:20 +0100

    finished exercice 2

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts 'Creating restaurants...'
+
+Restaurant.create!(
+  name: 'Pizza Yolo',
+  address: 'Paris',
+  phone_number: '+33615896314',
+  category: 'italian'
+)
+
+Restaurant.create!(
+  name: 'La Table',
+  address: 'Nantes',
+  phone_number: '+3396589658',
+  category: 'french'
+)
+
+Restaurant.create!(
+  name: 'McDonalds',
+  address: 'Marseille',
+  phone_number: '+33488548526',
+  category: 'belgian'
+)
+
+Restaurant.create!(
+  name: 'Le Restaurant',
+  address: 'Lille',
+  phone_number: '+33698547824',
+  category: 'chinese'
+)
+
+Restaurant.create!(
+  name: 'Sushi',
+  address: 'Strasbourg',
+  phone_number: '+33452855558',
+  category: 'japanese'
+)
+
+puts 'Finished!'
